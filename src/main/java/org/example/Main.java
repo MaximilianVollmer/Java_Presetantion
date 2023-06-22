f9f1fd703b7e6b06b9a10cd36565a11099c07871
Vollmer Max <maximilian.vollmer@iu-study.org>
2023-06-22 20:39:28 +0200

    first

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!, Hi Hasan");
+
+        System.out.println("test");
     }
 }
