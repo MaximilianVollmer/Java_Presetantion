f508766c51f3e9c02527ab58474dfa847212691b
Markus Franzen <markus.franzen@fischer-teamplan.de>
2023-06-23 09:52:54 +0200

    Adding delete and create functions for list-items. Adding constructors in those items.

--- Listen/Listentypen/Aufgaben.java
+++ Listen/Listentypen/Aufgaben.java
@@ -1,5 +1,13 @@
 package Java_Presetantion.Listen.Listentypen;
 
 public class Aufgaben {
-    
+    public int importance;
+    public String name;
+    public String description;
+
+    public Aufgaben(int importance, String name, String description){
+        this.name = name;
+        this.importance = importance;
+        this.description = description;
+    }
 }
--- Listen/Listentypen/Notizen.java
+++ Listen/Listentypen/Notizen.java
@@ -1,5 +1,11 @@
 package Java_Presetantion.Listen.Listentypen;
 
 public class Notizen {
-    
+    public String name;
+    public String informations;
+
+    Notizen(String name, String informations){
+        this.name = name;
+        this.informations = informations;
+    }
 }
