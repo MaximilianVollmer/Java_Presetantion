16bcf4cbe442b72d3cd3625b3ba820b94ac35757
Markus Franzen <markus.franzen@fischer-teamplan.de>
2023-06-23 09:52:54 +0200

    Adding delete and create functions for list-items. Adding constructors in those items.

--- Listen/Liste.java
+++ Listen/Liste.java
@@ -9,21 +9,49 @@ import Java_Presetantion.Listen.Listentypen.Notizen;
 public class Liste{
 
     public ArrayList<Einkaufsitems> Einkaufliste; 
-    public ArrayList<Aufgaben> Aufgabenlite;
+    public ArrayList<Aufgaben> Aufgabenliste;
     public ArrayList<Kontakte> Kontaktbuch;
     public ArrayList<Notizen> Notizbuch;
 
     Liste(){
-
+        //Hier muss er aus der JSON-Datei die Notizen, Aufgaben, usw. auslesen und dann in die jeweilige Variable einspeichern
     }
 
+    // Kann man evt. noch mit ner switch case und den Objectkeys vereinfachen.
     void newItem(String name, int amount, String description, String category){
         Einkaufsitems item = new Einkaufsitems(name, amount, description, category);
         this.Einkaufliste.add(item);
     }
 
+    void newTask(String name, int importance, String description){
+        Aufgaben item = new Aufgaben(importance, name, description);
+        this.Aufgabenliste.add(item);
+    }
+
+    void newContact(String name, int importance, String description){
+        Kontakte item = new Kontakte(importance, name, description);
+        this.Kontaktbuch.add(item);
+    }
+
+    void newNote(String name, int importance, String description){
+        Notizen item = new Notizen(importance, name, description);
+        this.Notizbuch.add(item);
+    }
+
+    void deleteNote(int index){
+        this.Notizbuch.remove(index-1);
+    }
+
+    void deleteContact(int index){
+        this.Kontaktbuch.remove(index-1);
+    }
+
+    void deleteTask(int index){
+        this.Aufgabenliste.remove(index-1);
+    }
+
     void deleteItem(int index){
-        this.Einkaufliste.remove(index);
+        this.Einkaufliste.remove(index-1);
     }
 
     public static void main(String[] args) {
--- Listen/Listentypen/Kontakte.java
+++ Listen/Listentypen/Kontakte.java
@@ -1,5 +1,13 @@
 package Java_Presetantion.Listen.Listentypen;
 
 public class Kontakte {
+    public String first_name;
+    public String last_name;
+    public int tel_number;
     
+    Kontakte(String first_name, String last_name, int tel_number){
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.tel_number = tel_number;
+    }
 }
