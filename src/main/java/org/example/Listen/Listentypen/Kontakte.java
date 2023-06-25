0f9014f7294efcb7773c101fe9ccecb56bfd8a01
Markus Franzen <markus.franzen@fischer-teamplan.de>
2023-06-25 20:39:18 +0200

    Adjust filepath

--- src/main/java/org/example/Listen/Listentypen/Kontakte.java
+++ src/main/java/org/example/Listen/Listentypen/Kontakte.java
@@ -1,11 +1,11 @@
-package Java_Presetantion.Listen.Listentypen;
+package org.example.Listen.Listentypen;
 
 public class Kontakte {
     public String first_name;
     public String last_name;
     public int tel_number;
     
-    Kontakte(String first_name, String last_name, int tel_number){
+    public Kontakte(String first_name, String last_name, int tel_number){
         this.first_name = first_name;
         this.last_name = last_name;
         this.tel_number = tel_number;
