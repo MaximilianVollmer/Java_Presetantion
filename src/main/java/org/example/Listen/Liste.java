e019f9e32b9a607d1ae1672a3a84eb31f40e8e67
Markus Franzen <markus.franzen@fischer-teamplan.de>
2023-06-27 16:01:07 +0200

    Adjust scanner function|Add functions to get informations from lists.

--- src/main/java/org/example/Listen/Liste.java
+++ src/main/java/org/example/Listen/Liste.java
@@ -2,6 +2,8 @@ package org.example.Listen;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.swing.undo.UndoableEdit;
+
 import org.example.Listen.Listentypen.Aufgaben;
 import org.example.Listen.Listentypen.Einkaufsitems;
 import org.example.Listen.Listentypen.Kontakte;
@@ -19,52 +21,83 @@ public class Liste{
     }
 
     // Kann man evt. noch mit ner switch case und den Objectkeys vereinfachen.
-    void newItem(String name, int amount, String description, String category){
+    public void newItem(String name, int amount, String description, String category){
         Einkaufsitems item = new Einkaufsitems(name, amount, description, category);
         this.Einkaufliste.add(item);
     }
 
-    void newTask(String name, int importance, String description){
+    public void newTask(String name, int importance, String description){
         Aufgaben item = new Aufgaben(importance, name, description);
         this.Aufgabenliste.add(item);
     }
 
-    void newContact(String first_name, String last_name, int tel_number){
+    public void newContact(String first_name, String last_name, int tel_number){
         Kontakte item = new Kontakte(first_name, last_name, tel_number);
         this.Kontaktbuch.add(item);
     }
 
-    void newNote(String name, String description){
+    public void newNote(String name, String description){
         Notizen item = new Notizen(name, description);
         this.Notizbuch.add(item);
     }
 
-    void deleteNote(int index){
+    public void deleteNote(int index){
         this.Notizbuch.remove(index-1);
     }
 
-    void deleteContact(int index){
+    public void deleteContact(int index){
         this.Kontaktbuch.remove(index-1);
     }
 
-    void deleteTask(int index){
+    public void deleteTask(int index){
         this.Aufgabenliste.remove(index-1);
     }
 
-    void deleteItem(int index){
+    public void deleteItem(int index){
         this.Einkaufliste.remove(index-1);
     }
 
-    public static void main(String[] args) {
-        Liste list = new Liste();
+    public void scanner_case(Liste listen, ArrayList<Einkaufsitems> list){
+        if(list != null && list.size()!=0){
+            for(int index = 0; index<=list.size(); index++){
+                System.out.println("["+index+1+"]"+list.get(index).get_informations());
+            }
+            System.out.println("Wählen sie das Item aus, um genauere Informationen zu sehen, es zu bearbeiten, oder zu löschen.");
+            Scanner scan_one = new Scanner(System.in);
+            String action_one = scan_one.nextLine();
+            System.out.println(list.get(Integer.parseInt(String.valueOf(action_one)))); 
+            scan_one.close();
+        }
+        else{
+            System.out.println("Sie haben nichts in ihrer Einkaufsliste.");
+            System.out.println("[1] Einen neuen Gegenstand der Liste hinzufügen.");
+            System.out.println("[2] Zurück");
+            Scanner scan_one = new Scanner(System.in);
+            String action_one = scan_one.nextLine();
 
+            switch(String.valueOf(action_one)){
+                case "1":
+                    // Scanner add_item = new Scanner(System.in);
+                    // String action_add_item = add_item.nextLine();
+                    // Hier muss funktion hin für Eintragen von Name und Beschreibung 
+                    // add_item.close();
+                    break;
+                case "2":
+                    listen.main_func(listen);
+                    break;
+            }
+            scan_one.close();
+        }
+    }
+
+    public void main_func(Liste list){
         System.out.println("[1] Öffne die Einkaufsliste\n[2] Öffne Aufgabenliste\n[3] Öffne Kontaktbuch \n[4] Öffne Notizbuch");
 
         Scanner new_scan = new Scanner(System.in);
         String new_action = new_scan.nextLine();
         switch(String.valueOf(new_action)){
             case "1":
-                System.out.println(list.Einkaufliste);
+                list.scanner_case(list, list.Einkaufliste);
                 break;
             case"2":
                 System.out.println(list.Aufgabenliste);
@@ -79,5 +112,10 @@ public class Liste{
                 System.out.println("Bitte geben sie nur einen der oben genannten Werte ein.");
         }
         new_scan.close();
+    } 
+
+    public static void main(String[] args) {
+        Liste list = new Liste();
+        list.main_func(list);
     }
 }
