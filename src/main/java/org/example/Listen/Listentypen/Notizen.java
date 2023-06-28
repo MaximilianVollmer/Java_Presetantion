72f5e1e6a229cea77cf383e92d20c70f80080ca6
Markus Franzen <markus.franzen@fischer-teamplan.de>
2023-06-28 16:24:18 +0200

    Adjust names|Adjust deletefunction/s|Add function to get all inforations|Somemore interaction abilities

--- src/main/java/org/example/Listen/Listentypen/Notizen.java
+++ src/main/java/org/example/Listen/Listentypen/Notizen.java
@@ -1,10 +1,14 @@
 package org.example.Listen.Listentypen;
 
-public class Notizen extends Liestentyp{
+public class Notizen extends Listentyp{
     public String informations;
 
     public Notizen(String name, String informations){
         this.name = name;
         this.informations = informations;
+    }
+    public String all_informations(){
+        String info = "Name: "+this.name +"\nInhalt: "+this.informations;
+        return info;
     }
 }
