4747d82237fbe82bdc80f4cc8180d20fd00b53ac
Vollmer Max <maximilian.vollmer@iu-study.org>
2023-06-29 08:55:30 +0200

    Kalender-Klassen hizugefügt und Json-Library

---
+++ src/Kalender/Stundenplan/Vorlesung.java
@@ -0,0 +1,11 @@
+package Kalender.Stundenplan;
+
+import Kalender.KalenderElements.KalenderElement;
+
+import java.util.Date;
+
+public class Vorlesung extends KalenderElement {
+    public Vorlesung(String name, Date date){
+
+    }
+}
