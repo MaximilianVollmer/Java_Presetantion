14cf6d7763a9fd9b2106755006398ec66ecafcfb
Vollmer Max <maximilian.vollmer@iu-study.org>
2023-06-30 02:35:27 +0200

    Tweak the Maven-Stuff

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.json.simple.parser.JSONParser;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!, Hi Hasan");
+        JSONParser test = new JSONParser();
     }
 }
