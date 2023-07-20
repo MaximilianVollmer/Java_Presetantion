526ae1beff2ffd91ccb3771d58e9ede26849e1d4
Vollmer Max <maximilian.vollmer@iu-study.org>
2023-07-20 14:50:34 +0200

    Add generateDateTime Method

--- src/main/java/org/example/Generators/TimeGenerator.java
+++ src/main/java/org/example/Generators/TimeGenerator.java
@@ -9,17 +9,18 @@ public class TimeGenerator {
     /* TimeGenerators generateDate() Method can be called statically
     to return a LocalDateTime Object based on the user Input
     This is used for the "Stundenplan" Class to get a time for a lesson */
+
     private static DayOfWeek weekdayToEnum(String weekday){
 
         return switch (weekday.toLowerCase()) {
-            case "monday" -> DayOfWeek.MONDAY;
-            case "tuesday" -> DayOfWeek.TUESDAY;
-            case "wednesday" -> DayOfWeek.WEDNESDAY;
-            case "thursday" -> DayOfWeek.THURSDAY;
-            case "friday" -> DayOfWeek.FRIDAY;
-            case "saturday" -> DayOfWeek.SATURDAY;
-            case "sunday" -> DayOfWeek.SUNDAY;
-            default -> throw new StackOverflowError("Invalid weekday!");
+            case "montag" -> DayOfWeek.MONDAY;
+            case "dienstag" -> DayOfWeek.TUESDAY;
+            case "mittwoch" -> DayOfWeek.WEDNESDAY;
+            case "donnerstag" -> DayOfWeek.THURSDAY;
+            case "freitag" -> DayOfWeek.FRIDAY;
+            case "samstag" -> DayOfWeek.SATURDAY;
+            case "sonntag" -> DayOfWeek.SUNDAY;
+            default -> throw new StackOverflowError("Kein Wochentag!");
         };
     }
 
@@ -27,39 +28,60 @@ public class TimeGenerator {
 
         int hour = (int)time;
         int minute = ((int)Math.round(time*100)) % 100;
+        double twoDigitTime = (Math.floor(time*100))/100;
 
         if( (hour <= 23 && hour >= 0) && (minute <= 59 && minute >= 0) ){
-            return (Math.floor(time*100))/100;
+            return twoDigitTime;
         }
 
-        throw new Exception("Invalid time!");
+        throw new Exception("Keine zulässige Zeit!");
     }
 
-    public static LocalDateTime GenerateDate(){
+    public static LocalDateTime generateWeekdayTime(){
 
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         try {
-            System.out.println("What weekday?");
+            System.out.println("An welchem Wochentag?");
             DayOfWeek weekday = weekdayToEnum(scanner.nextLine());
 
-            System.out.println("What time?");
+            System.out.println("Um welche Uhrzeit? (HH.mm)");
             double time = TimeGenerator.doubleValidator(scanner.nextDouble());
 
             LocalDate date = LocalDate.now();
-            return LocalDateTime.of(date.with(weekday).plusWeeks(1),LocalTime.of((int)time,((int)Math.round(time*100)) % 100));
+
+            boolean weekdayBeforeDate = date.with(weekday).isBefore(LocalDate.now());
+
+            return LocalDateTime.of(!weekdayBeforeDate ? date.with(weekday) : date.with(weekday).plusWeeks(1) ,LocalTime.of((int)time,((int)Math.round(time*100)) % 100));
         }
         catch(StackOverflowError | Exception e){
             System.out.println(e.getMessage());
-            return GenerateDate();
+            return generateWeekdayTime();
+        }
+    }
+
+    public static LocalDateTime generateDateTime(){
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.M.yyyy HH:mm");
+
+        try {
+            System.out.println("An welchem Datum?");
+
+            LocalDateTime date = LocalDateTime.parse(scanner.nextLine() + " 00:00", formatter);
+
+            return date;
+        }
+        catch(StackOverflowError | Exception e){
+            System.out.println(e.getMessage());
+            return generateDateTime();
         }
     }
 
     public static void main(String[] args){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
-        LocalDateTime date = GenerateDate();
 
-        System.out.print(date.getDayOfWeek()+" ");
-        System.out.println(date.format(formatter));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.M.yyyy");
+
+        System.out.println(generateDateTime().format(formatter));
     }
 }
