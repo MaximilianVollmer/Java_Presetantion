b647c3c99ca0ae2d21678503eb6f2fb7c5fa50b2
Maximilian Vollmer <106180729+MaximilianVollmer@users.noreply.github.com>
2023-07-27 11:16:43 +0200

    Hasan develop (#29)
    * test commit
    
    * Adding Listen and update main, so it works
    
    * Just some stuff
    
    * first
    
    * fix?
    
    * fix?
    
    * Eventsklasse
    
    * Adjust Reminder
    
    * meine letzten Änderungen!
    
    * perfektionst
    
    ---------
    
    Co-authored-by: Markus Franzen <markus.franzen@fischer-teamplan.de>
    Co-authored-by: HAsanOSama2000 <hasanabourra@gmail.com>
    Co-authored-by: Abouorra <HAbouorra@kramerundcrew.de>

---
+++ Listen/Listentypen/Einkaufsitems.java
@@ -0,0 +1,16 @@
+package Java_Presetantion.Listen.Listentypen;
+
+public class Einkaufsitems {
+
+    public String name;
+    public int amount;
+    public String description;
+    public String category;
+
+    public Einkaufsitems(String name, int amount, String description, String category){
+        this.name = name;
+        this.amount = amount;
+        this.description = description;
+        this.category = category;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,7 +12,5 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!, Hi Hasan");
-        LocalDateTime date = TimeGenerator.generateDateTime();
-
     }
 }
