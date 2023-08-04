1fa212420927f225045d93114c647a0e491104ea
Abouorra <HAbouorra@kramerundcrew.de>
2023-08-04 09:25:57 +0200

    hallo

--- src/main/java/org/example/Events/Reminder.java
+++ src/main/java/org/example/Events/Reminder.java
@@ -9,12 +9,13 @@ import java.util.TimerTask;
 
 public class Reminder extends Events{
      static LocalDateTime event_date = LocalDateTime.now();
-
      
   
     public Reminder(String event_name, LocalDateTime event_start, LocalDateTime event_end, String event_priority) {
         super(event_name, event_start, event_end, event_priority);
-
+        //TODO Auto-generated constructor stub
+       // You can also convert the difference to minutes, hours, or any other time unit as needed.
+        // For example, to get the difference in minutes:
         long diffInMinutes = Duration.between(LocalDateTime.now(), event_start).toMillis();
         Reminder this_old = this;
         int diffInMinutesAsInt = Math.toIntExact(diffInMinutes);
@@ -23,7 +24,8 @@ public class Reminder extends Events{
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                Reminder.Reminder_info(this_old);
+              //  Reminder.Reminder_info(this_old);
+              Reminder.Reminder_sleep( event_start, this_old);
             }
         }, diffInMinutesAsInt, 60000);
 
@@ -41,56 +43,42 @@ public class Reminder extends Events{
 
     }
 
-    // static  DateTimeFormatter dtf = DateTimeFormatter.ofPattern();
-
-       
-    static void reminder_sleep (LocalDateTime time, Events rem){
-       // String patter = "dd/MM/yyyy HH:mm";
-        //DateTimeFormatter dtf = DateTimeFormatter.ofPattern(patter);
-        //event_date = time;
-
-        if (time.isBefore(LocalDateTime.now())){
-            //System.out.println(dtf.format(time));
-            time.plusMinutes(Long.parseLong("1"));
-           // System.out.println(LocalDateTime.now().plusMinutes(Long.parseLong("5")));
-         //   reminder_sleep(LocalDateTime.now().plusMinutes(Long.parseLong("5")));
-         // case 1 : true = sleeper +5 min
-             // Rekursiver Aufruf der Methode nach 5 Minuten
-            Reminder_info(rem);
-            System.out.println("[0] Erinnerung löschen");
-            System.out.println("[1] Erinnerung schlummern");
-            Scanner scan = new Scanner(System.in);
-            try{
-                while(scan.hasNextLine()){
-                    String action = scan.nextLine();
-                    switch(String.valueOf(action)){
-
-                        case "0":
+    static void Reminder_sleep(LocalDateTime time, Events rem) {
+    if (time.isBefore(LocalDateTime.now())) {
+        Reminder_info(rem);
+        System.out.println("[0] Erinnerung löschen");
+        System.out.println("[1] Erinnerung schlummern");
+        System.out.println("[2] Zurück"); // Hinzufügen einer Zurück-Option
+        Scanner scan = new Scanner(System.in);
+        boolean runner = true ;
+        try {
+            while (runner) {
+                String action = scan.nextLine();
+                switch (String.valueOf(action)) {
+                    case "0":
+                    runner =false;
                         System.out.println("Erinnerung wurde gelöscht");
-                            break;
-                        case "1" :
+                        break; // Beende die Timer-Aufgabe und die Methode
+                    case "1":
                         System.out.println("Erinnerung wurde um 5 Minuten verlängert");
-                        reminder_sleep(time.plusMinutes(5), rem);
-                        
+                        time = time.plusMinutes(5);
                         break;
-
-
-
-
-                    }
+                    case "2":
+                        System.out.println("Zurück");
+                        break; // Beende die Schleife und die Methode
                 }
-            } 
-            catch(Exception e) {
-                System.out.println(e);
             }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
-            try {
-                Thread.sleep(1 * 60 * 1000); // Warte 5 Minuten (in Millisekunden)
-    
-                reminder_sleep(time,rem);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        try {
+            Thread.sleep(1 * 60 * 1000); // Warte 5 Minuten (in Millisekunden)
+            Reminder_sleep(time, rem);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
+}
+
 }
